b37b9cb470cab53574f720f9bf7c5852f66e01a3
Amarjeet <amarjeetpatel25112002@gmail.com>
2024-09-17 16:38:02 +0530

    frontpage add

--- E-learning-frontend/src/App.jsx
+++ E-learning-frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ComponentsRouter from './ComponentsRouter';
+import Navbar from './components/Auth/Navbar';
+import Footer from './components/Auth/Footer';
+import Banner from './components/Auth/Banner';
 
 function App() {
-  return (
+  return (<>
+    <Navbar/>
+    <Footer/>
+    <Banner/>
     <div className='min-h-screen grid place-content-center bg-gray-100 p-4 sm:p-8 md:p-12 lg:p-16'>
       <ComponentsRouter />
     </div>
+    </>
   );
 }
 
---
+++ E-learning-frontend/src/components/Auth/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+function Navbar() {
+  return (<>
+    <div className='max-w-screen-2xl container mx-auto md:px-15 px-4'>
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Home</a></li>
+        <li><a>Courses</a></li>
+       
+        </ul>
+    </div>
+    <a className=" text-2xl cursor-pointer font-bold">E-learning</a>
+  </div>
+  <div className="navbar-end space-x-3">
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>Courses</a></li>
+      
+    </ul>
+  </div>
+  <div className='hidden md:block'>
+  <label className="px-3 py-2 border rounded-md flex items-center gap-2">
+  <input type="text" className="grow outline-none" placeholder="Search" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 15 15"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+</label>
+  </div>
+  <div className="">
+    <a className="bg-black text-white p-2 rounded-md hover:bg-stone-800 duration-300 cursor-pointer">Login</a>
+  </div>
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+      <div className="w-10 rounded-full">
+        <img
+          alt="Tailwind CSS Navbar component"
+          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+      </div>
+    </div>  
+</div>
+</div>
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default Navbar
