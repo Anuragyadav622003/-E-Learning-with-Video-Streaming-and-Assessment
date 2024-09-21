0bafc7fbe4c4ad3f4bb6ed45f2f6a6d0bcc6f724
Amarjeet <amarjeetpatel25112002@gmail.com>
2024-09-21 10:46:53 +0530

    new

--- E-learning-frontend/src/App.jsx
+++ E-learning-frontend/src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Banner from './components/Banner';
 import { BrowserRouter } from 'react-router-dom';
 import ComponentsRouter from './ComponentsRouter';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
 
 function App() {
   return (<>
     <div>
-     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
+    <BrowserRouter>
+    <div className="min-h-screen flex flex-col">
+    <Navbar />
+    <Banner/>
         <main className="flex-grow flex items-center justify-center container mx-auto p-4">
           <ComponentsRouter />
         </main>
+        <Footer/>
       </div>
     
      </BrowserRouter>
-    <Banner/>
-    <Footer/>
     </div>
     </>
   );
